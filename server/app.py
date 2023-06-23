b85f11d6fbb8302d8c6c240e8431e2685b368e23
daniel <sirkaranja12@gmail.com>
2023-06-23 14:03:06 +0300

    flask-alchemy'

--- server/app.py
+++ server/app.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
 # app/app.py
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
+from models import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy()  # Instantiate the SQLAlchemy object without app argument
-
 migrate = Migrate(app, db)
 
-db.init_app(app)  # Register SQLAlchemy on the Flask app
+db.init_app(app)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(port=5555, debug=True)
+    app.run(port=5504)
