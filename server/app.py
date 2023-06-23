8b3b9abe94f5e29d3acba4784dde47973cac1c92
daniel <sirkaranja12@gmail.com>
2023-06-23 13:53:21 +0300

    flask-alchemy

--- server/app.py
+++ server/app.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
+# app/app.py
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy()  # Instantiate the SQLAlchemy object without app argument
+
+migrate = Migrate(app, db)
+
+db.init_app(app)  # Register SQLAlchemy on the Flask app
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(port=5555, debug=True)
---
+++ server/migrations/versions/6d0452819ac1_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 6d0452819ac1
+Revises: 
+Create Date: 2023-06-23 13:48:09.162698
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6d0452819ac1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
